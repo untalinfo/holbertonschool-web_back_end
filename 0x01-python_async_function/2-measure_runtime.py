684cfa0932c8d947dc37ca3e1787365ad5564e8a
Oskar Info <racso1607@gmail.com>
2021-04-27 15:37:08 -0500

    task 2 added

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
+"""
+Measure the runtime
+"""
+import asyncio
+import time
 
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measures the total execution time for wait_n(n, max_delay),
+    and returns total_time / n. Your function should return a float.
+
+    Args:
+        n (int): number of repetitions
+        max_delay (int): delay
+
+    Returns:
+        float: total time / n
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    return total_time / n
