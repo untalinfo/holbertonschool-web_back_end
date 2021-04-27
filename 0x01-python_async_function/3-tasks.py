64438d609c4d0908c96ca18c0cbef0ee297c33d6
Oskar Info <racso1607@gmail.com>
2021-04-27 15:47:04 -0500

    fix return function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import time
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """takes an integer max_delay and returns a asyncio.Task.
 
     Args:
