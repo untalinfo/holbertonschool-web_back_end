02d8854f78a14aaa3cdbc72f942004f198f04ea1
Oskar Info <racso1607@gmail.com>
2021-04-27 12:58:00 -0500

    fix ramdon uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
+"""
+The basics of async
+"""
 import random
 import asyncio
 
 
-async def wait_random(max_delay = 10):
-    await asyncio.sleep(random.randrange(0, max_delay))
+async def wait_random(max_delay: int = 10) -> float:
+    """takes in an integer argument (max_delay, with a default value of 10)
+    named wait_random that waits for a random delay between 0 and max_delay
+    (included and float value) seconds and eventually returns it.
+
+    Args:
+        max_delay (int, optional): delay. Defaults to 10.
+
+    Returns:
+        [type]: [description]
+    """    
+    await asyncio.sleep(random.uniform(0, max_delay))
     return max_delay
