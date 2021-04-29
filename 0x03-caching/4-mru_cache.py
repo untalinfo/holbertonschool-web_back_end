c6eed67c4fc878f68c1eaefef5c730c6d8dda08c
Oskar Info <racso1607@gmail.com>
2021-04-29 13:41:59 -0500

    task4 added

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+MRU Caching
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Base MRU class inhert BaseCaching
+    """
+    def __init__(self):
+        """
+        Constructor Method
+        """
+        super().__init__()
+        self.list_name = []
+
+    def put(self, key, item):
+        """
+        Put Method
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if key not in self.list_name:
+            self.list_name.append(key)
+        else:
+            if self.list_name[-1] != key:
+                self.list_name.remove(key)
+                self.list_name.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.list_name[-2]
+            print('DISCARD: {}'.format(discard))
+            del self.cache_data[discard]
+            self.list_name.pop(-2)
+
+    def get(self, key):
+        """
+        Get Method
+        """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.list_name:
+            if self.list_name[-1] != key:
+                self.list_name.remove(key)
+                self.list_name.append(key)
+
+        return self.cache_data[key]
