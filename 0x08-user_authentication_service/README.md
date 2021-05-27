d83fc8b5bd1f7ab3bc09deb7b5df525c89f5b1f0
untalinfo <racso1607@gmail.com>
2021-05-27 00:38:50 -0500

    initial commit

---
+++ 0x08-user_authentication_service/README.md
@@ -0,0 +1,9 @@
+# 0x08. User authentication service
+
+## Resources
+* [Flask documentation](https://flask.palletsprojects.com/en/1.1.x/quickstart/)
+* [Requests module](https://requests.kennethreitz.org/en/master/user/quickstart/)
+* [HTTP status codes](https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html)
+
+* You will need to install bcrypt
+ ~ pip3 install bcrypt
---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+from typing import TypeVar
+
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self):
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+            Saves the user in the database, does not
+            perform validation
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+            Takes a kwargs and returns the first row
+            found in the users table
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            use the find_user_by method to search
+            and update the data passed in the method
+        """
+        user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
