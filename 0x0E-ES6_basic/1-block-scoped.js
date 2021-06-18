b08402cb0c759fcca1d4c80b2fb79bb42ff6b8f4
untalinfo <racso1607@gmail.com>
2021-06-17 21:55:01 -0500

    fix

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-
+  
     if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
+      /*eslint-disable */
+      const task = true;
+      const task2 = false;
+      /* eslint-enable */
     }
-
+  
     return [task, task2];
-}
+  }
