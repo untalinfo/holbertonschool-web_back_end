9e9257b4cf798dc2302512d2f8956c559be74abd
untalinfo <racso1607@gmail.com>
2021-06-23 09:23:12 -0500

    fix

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,8 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+async function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  const signIn = await signUpUser(firstName, lastName)
+    .then((response) => ({
+      status: 'fulfilled',
+      value: response,
+    }))
+    .catch((e) => ({
+      status: 'rejected',
+      value: e.toString(),
+    }));
+  const upload = await uploadPhoto(fileName)
+    .catch((e) => ({
+      status: 'rejected',
+      value: e.toString(),
+    }));
+
+  return [signIn, upload];
 }
 
 export default handleProfileSignup;
