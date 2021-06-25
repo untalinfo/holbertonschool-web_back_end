8e7eca82f80ea1fa2d97d02f5d5498c11a075f8e
untalinfo <racso1607@gmail.com>
2021-06-24 23:00:58 -0500

    task pricing added

---
+++ 0x10-ES6_classes/4-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/extensions */
+import Pricing from './4-pricing.js';
+import Currency from './3-currency.js';
+
+const p = new Pricing(100, new Currency('EUR', 'Euro'));
+console.log(p);
+console.log(p.displayFullPrice());
---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
